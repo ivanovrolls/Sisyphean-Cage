9e48bcf0c8b7e2f4742f260f7e9b44377248c6d8
ivanovrolls <igermanoff2@gmail.com>
2026-06-16 10:49:11 +0100

    Basic analysis pipeline

---
+++ sampling.py
@@ -0,0 +1,15 @@
+import json
+
+with open('claude_2023.json', 'r') as file:
+    data = file.read()
+
+parsed = json.loads(data)
+principles = 0
+
+for section in parsed["sections"]:
+    principles_in_section = len(section["principles"])
+    print(section["name"], ": ", principles_in_section)
+    principles += len(section["principles"])
+
+sections = len(parsed["sections"])
+print("Sections: ", sections, "Principles: ", principles)
